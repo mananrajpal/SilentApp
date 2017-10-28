8f23351f1b64c34c8f403fed1f48a04c6c0f5b5b
Manan Rajpal <100864824@student.swin.edu.au>
2017-10-29 06:09:51 +1100

    First page layout designed

--- app/src/main/java/com/example/rajpa/silentapplication/Access.java
+++ app/src/main/java/com/example/rajpa/silentapplication/Access.java
@@ -1,7 +1,12 @@
 package com.example.rajpa.silentapplication;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Access extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class Access extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_access);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar); //setting the custom Toolbar
+        setSupportActionBar(myToolbar);
+        initializeUI();
+    }
+
+    private void initializeUI()
+    {
+      /*------------------------------------------------------------------------------------------*/
+        //setting up objects with respective to their xml views.
+        ActionBar myActionBar = getSupportActionBar();
+        TextView studentAccess = (TextView)findViewById(R.id.StudentAccess);
+        TextView staffAccess = (TextView)findViewById(R.id.StaffAccess);
+        /*----------------------------------------------------------------------------------------*/
+
+        myActionBar.setLogo(R.drawable.silent); //setting up the logo on ActionBar
+
+        /*setting up onClickListener to respective textViews.*/
+        studentAccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Entering Student Access", Toast.LENGTH_LONG).show();
+            }
+        });
+        staffAccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Entering Staff Access", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
