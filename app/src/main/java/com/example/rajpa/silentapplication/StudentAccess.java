6d40db6d44b403dbddd5996d7e4602d543bd60f4
Manan Rajpal <100864824@student.swin.edu.au>
2017-10-29 04:55:57 +1100

    First commit of the raw project

---
+++ app/src/main/java/com/example/rajpa/silentapplication/StudentAccess.java
@@ -0,0 +1,13 @@
+package com.example.rajpa.silentapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentAccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_access);
+    }
+}
