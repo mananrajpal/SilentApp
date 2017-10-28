378d15894f1ff19cec44584470e4b807d1e8774c
Manan Rajpal <100864824@student.swin.edu.au>
2017-10-29 07:30:20 +1100

    Final commit before mergin to master

--- app/src/main/java/com/example/rajpa/silentapplication/StudentAccess.java
+++ app/src/main/java/com/example/rajpa/silentapplication/StudentAccess.java
@@ -1,9 +1,11 @@
 package com.example.rajpa.silentapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 public class StudentAccess extends AppCompatActivity {
@@ -22,9 +24,34 @@ public class StudentAccess extends AppCompatActivity {
         /*------------------------------------------------------------------------------------------*/
         //setting up objects with respective to their xml views.
         ActionBar myActionBar = getSupportActionBar();
+        final TextView createQR = (TextView) findViewById(R.id.CreateQr);
+        TextView scanDevices = (TextView) findViewById(R.id.ScanDevices);
+        TextView challenge = (TextView) findViewById(R.id.Challenge);
         /*----------------------------------------------------------------------------------------*/
 
         myActionBar.setLogo(R.drawable.silent); //setting up the logo on ActionBar
 
+        /*Setting up the onClickListener for respective text fields.*/
+        createQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getApplicationContext(),createQR.class);
+                startActivity(myIntent);
+            }
+        });
+
+        scanDevices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        challenge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        /*----------------------------------------------------------------------------------------*/
     }
 }
---
+++ app/src/main/java/com/example/rajpa/silentapplication/createQR.java
@@ -0,0 +1,55 @@
+package com.example.rajpa.silentapplication;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.oned.MultiFormatOneDReader;
+
+public class createQR extends AppCompatActivity {
+    String androidId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_qr);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        initializeUI();
+    }
+
+    private void initializeUI()
+    {
+        /*------------------------------------------------------------------------------------------*/
+        //setting up objects with respective to their xml views.
+        ActionBar myActionBar = getSupportActionBar();
+        /*----------------------------------------------------------------------------------------*/
+        myActionBar.setLogo(R.drawable.silent); //setting up the logo on ActionBar
+        androidId = getAndroidId();
+        createQR();
+
+        Log.d("Id-Testing", androidId);
+    }
+
+    private String getAndroidId()
+    {
+        String id;
+        id = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+        return id;
+    }
+
+    private void createQR()
+    {
+        
+    }
+
+}
