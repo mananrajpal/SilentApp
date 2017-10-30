a771ed38ee3dbfa256329e5768b57679f9739f7d
Manan Rajpal <100864824@student.swin.edu.au>
2017-10-31 01:52:37 +1100

    Fixed bugs, Checks internet connection before login

--- app/src/main/java/com/example/rajpa/silentapplication/StaffAccess.java
+++ app/src/main/java/com/example/rajpa/silentapplication/StaffAccess.java
@@ -1,15 +1,22 @@
 package com.example.rajpa.silentapplication;
 
 import android.app.FragmentTransaction;
+import android.app.Service;
+import android.content.ServiceConnection;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
 
 public class StaffAccess extends AppCompatActivity implements StaffLogin.loginListener {
 
+
     @Override
-    public void loginStaff(String name, String pass) {
+    public void loginStaff(Boolean validated) {
 
     }
 
@@ -26,10 +33,12 @@ public class StaffAccess extends AppCompatActivity implements StaffLogin.loginLi
     {
         ActionBar myBar = getSupportActionBar();
         myBar.setLogo(R.drawable.silent);
-
         StaffLogin fragment1= new StaffLogin();
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.staffFragment,fragment1);
         fragmentTransaction.commit();
+
     }
+
+
 }
--- app/src/main/java/com/example/rajpa/silentapplication/StaffLogin.java
+++ app/src/main/java/com/example/rajpa/silentapplication/StaffLogin.java
@@ -1,28 +1,44 @@
 package com.example.rajpa.silentapplication;
 
 import android.app.Activity;
+import android.app.Service;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
-public class StaffLogin extends Fragment {
+public class StaffLogin extends Fragment
+{
+    Boolean isInternetConnected = false;
     Boolean validated = false;
     loginListener mListener;
     public interface loginListener
     {
-        public void loginStaff(String name, String pass);
+        public void loginStaff(Boolean validated);
     }
     public StaffLogin() {
         // Required empty public constructor
@@ -54,41 +70,148 @@ public class StaffLogin extends Fragment {
 
 
         /*----------------------Starting the threading of login with onClickListener--------------*/
-        checkCredentials.setOnClickListener(new View.OnClickListener() {
+        checkCredentials.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
-                /*-------------------getting the associated views from xml file-------------------*/
-                EditText nameEntered = (EditText) getActivity().findViewById(R.id.userName);
-                EditText passEntered = (EditText) getActivity().findViewById(R.id.userPass);
-                /*----------------Converting the input to string------------------------------*/
-                String userName = nameEntered.getText().toString();
-                String userPass = passEntered.getText().toString();
-                String type = "login"; //Operation type is login
-                /*--------------------------------------------------------------------------------*/
-                /*---------------Validating data using TextUtils Library--------------------------*/
-                if(TextUtils.isEmpty(userName))
+
+                ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Service.CONNECTIVITY_SERVICE);
+                if(connectivityManager != null)
                 {
-                    nameEntered.setError("Enter name");
-                    return;
+                    NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+                    if(info != null)
+                    {
+                        if(info.getState()  == NetworkInfo.State.CONNECTED)
+                        {
+                            /*-----------------getting the associated views from xml file---------*/
+                            EditText nameEntered = (EditText) getActivity().findViewById(R.id.userName);
+                            EditText passEntered = (EditText) getActivity().findViewById(R.id.userPass);
+                        /*--------------------Converting the input to string----------------------*/
+                            String userName = nameEntered.getText().toString();
+                            String userPass = passEntered.getText().toString();
+                            String type = "login"; //Operation type is login
+                        /*------------------------------------------------------------------------*/
+                         /*---------------Validating data using TextUtils Library-----------------*/
+                            if(TextUtils.isEmpty(userName))
+                            {
+                                nameEntered.setError("Enter name");
+                                return;
+                            }
+                            if(TextUtils.isEmpty(userPass))
+                            {
+                                passEntered.setError("Enter password");
+                                return;
+                            }
+                            else
+                            {
+                                //performing the async threading of getting records from database
+                                BackgroundWorker br = new BackgroundWorker();
+                                br.execute(type, userName,userPass);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Toast.makeText(getActivity(),"Please connect to Internet",Toast.LENGTH_LONG).show();
+                    }
                 }
-                if(TextUtils.isEmpty(userPass))
+            }
+        });
+        return v;
+    }
+
+
+
+
+
+    class BackgroundWorker extends AsyncTask<String, Void, String>
+    {
+
+
+    /*Database is created as a local host not on a webserver which is why the url is set to static
+    * local host address.*/
+
+
+
+        String login_url = "http://discoloured-pops.000webhostapp.com/login.php";
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String aVoid) {
+            super.onPostExecute(aVoid);
+            String access = aVoid.substring(0,7);
+            Log.d("Checking-connection","Inside the post executed");
+            if(access.equals("Welcome"))
+            {
+                Log.d("Result-string","status");
+            }
+            Toast.makeText(getActivity(),aVoid,Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            //getting the operation type
+            String type = params[0];
+            if(type=="login")
+            {
+                String username = params[1];
+                String password = params[2];
+                Log.d("Checking-connection","Inside the connection");
+                try
                 {
-                    passEntered.setError("Enter password");
-                    return;
-                }
-                else
+                    URL url = new URL(login_url); //basic url created from the string.
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");//request method set as post to get value from php
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    Log.d("Checking-connection","After the output stream");
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                    Log.d("Checking-connection","After the buffered writer");
+                    String post_data = URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")+"&"
+                            +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                    bufferedWriter.write(post_data);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    Log.d("Checking-connection","After the input stream");
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                    Log.d("Checking-connection","After the bufferred reader");
+                    String result="", line="";
+                    while((line = bufferedReader.readLine()) != null)
+                    {
+                        result +=  line;
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    Log.d("Checking-connection",result);
+                    return result;
+                }catch (MalformedURLException e)
                 {
-                    //performing the async threading of getting records from database
-                    BackgroundWorker br = new BackgroundWorker(v);
-                    br.execute(type, userName,userPass);
+                    Toast.makeText(getContext(), "Problem with creating URL",Toast.LENGTH_LONG).show();
+                }catch(IOException e)
+                {
+                    //this catch block added for the HTTPURL connection
+                    Toast.makeText(getContext(), "Problem with Connecting to Internet",Toast.LENGTH_LONG).show();
                 }
 
             }
-        });
+            return null;
+        }
 
 
 
-        return v;
     }
+
 
 }
