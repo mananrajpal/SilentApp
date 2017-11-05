4edc20a2b9dad5eef76b9efdc3aaaa5cbcbe6ec6
Manan Rajpal <100864824@student.swin.edu.au>
2017-11-05 15:37:11 +1100

    Implemented update of score on database if answer is right

--- app/src/main/java/com/example/rajpa/silentapplication/TakeChallenge.java
+++ app/src/main/java/com/example/rajpa/silentapplication/TakeChallenge.java
@@ -1,5 +1,7 @@
 package com.example.rajpa.silentapplication;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -13,21 +15,29 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.aztec.encoder.Encoder;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 
 public class TakeChallenge extends Fragment
 {
     String question = "", readLine="";
+    Boolean isCorrect = false;
     public TakeChallenge() {
         // Required empty public constructor
     }
@@ -89,6 +99,7 @@ public class TakeChallenge extends Fragment
     }
 
 
+
     class GetAnswer extends AsyncTask<Void, Void, String>
     {
         String website;
@@ -96,16 +107,26 @@ public class TakeChallenge extends Fragment
         protected void onPreExecute()
         {
             super.onPreExecute();
-            website = "http://discoloured-pops.000webhostapp.com/GetDeviceOnCall.php";
+            website = "http://discoloured-pops.000webhostapp.com/GetAnswer.php";
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             Log.d("Store-question", "Answer:" +s);
+            EditText answerField = (EditText)getActivity().findViewById(R.id.answer);
+            String customAnswer = answerField.getText().toString();
+            if(customAnswer.equals(s))
+            {
+                new UpdateScore().execute();
+                Toast.makeText(getActivity(), "Congratulation you are one step closer to the award.",Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(getActivity(), "Wrong Answer, try again next time",Toast.LENGTH_SHORT).show();
+            }
 
         }
-
 
         @Override
         protected String doInBackground(Void... params) {
@@ -134,6 +155,61 @@ public class TakeChallenge extends Fragment
                 e.printStackTrace();
             }
 
+            return null;
+        }
+    }
+
+    class UpdateScore extends AsyncTask<Void, Void, String>
+    {
+        String website, deviceId;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            website = "http://discoloured-pops.000webhostapp.com/UpdateScore.php";
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            deviceId = bluetoothAdapter.getAddress();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("Store-question",s);
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try
+            {
+                URL url = new URL(website);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setRequestMethod("POST");
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("deviceId", "UTF-8")+ "="+ URLEncoder.encode(deviceId,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String line="", result = "";
+                while((line = bufferedReader.readLine())!=null)
+                {
+                    result = line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            }catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+            }catch (IOException e)
+            {
+                e.printStackTrace();
+            }
             return null;
         }
     }
